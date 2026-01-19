82c6a860908573a59c79f481418eadecbf3e2523
rpworksmods <kyej1023@gmail.com>
2026-01-19 17:35:28 +0000

    Delete, Clear, Equals
    - Added delete button functionality
    - Added clear all button functionality
    - Added equals button functionality
    - All equations now show up in the display field
    - Able to perform equations on the sum of the previous equation

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 // DOM References
 const numButtons = document.querySelectorAll(".buttons > .number");
 const opButtons = document.querySelectorAll(".buttons > .operator")
+const displayEquals = document.querySelector(".result")
+const displayA = document.querySelector("#a")
+const displayOperator = document.querySelector("#operator")
+const displayB = document.querySelector("#b")
 
 // Global variables
 let a = null
 let b = null
 let operator = null
+let sum = null
+
+// Display function
+function display() {
+    if (a != null) { displayA.innerText = a; }
+    if (operator != null) { displayOperator.innerText = operator; }
+    if (b != null) { displayB.innerText = b; }
+
+    if (a != null && b != null && operator != null) {
+        operate()
+    }
+}
 
 // Operation functions
 function operate() {
     let val = 0
-    console.log(a)
-    console.log(b)
-    console.log(operator)
     if (a != null && b != null && operator != null) {
         switch(operator) {
             case "+":
@@ -29,10 +42,49 @@ function operate() {
                 break;
         }
 
-        console.log(val)
+        displayEquals.innerText = "= " + val;
+        sum = val;
     }
 
 };
+
+function equals() {
+    displayA.innerText = sum;
+    displayOperator.innerText = "";
+    displayB.innerText = "";
+    displayEquals.innerText = "";
+
+    a = null;
+    b = null;
+    operator = null;
+};
+
+function clearEquation() {
+    a = null;
+    b = null;
+    operator = null;
+    sum = null;
+
+    displayA.innerText = "";
+    displayOperator.innerText = "";
+    displayB.innerText = "";
+    displayEquals.innerText = "";
+}
+
+function backspace() {
+    if (a != null && b != null) {
+        b = null;
+        displayB.innerText = "";
+        displayEquals.innerText = "";
+
+    } else if (a != null && b == null) {
+        operator = null;
+        a = null;
+        displayA.innerText = "";
+        displayOperator.innerText = "";
+        displayEquals.innerText = "";
+    }
+}
 
 // Click listeners
 numButtons.forEach(btn => {
@@ -44,7 +96,7 @@ numButtons.forEach(btn => {
         } else {
             b = parseInt(id);
         }
-
+        display()
     })
 });
 
@@ -52,7 +104,9 @@ opButtons.forEach(btn => {
     btn.addEventListener("click", (e) => {
         const id = e.target.id;
 
-        operator = id;
+        if (sum != null) { a = sum; }
 
+        operator = id;
+        display()
     })
 });

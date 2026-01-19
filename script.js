d3725915e56947cd6911feed7484ec4636ffb43b
rpworksmods <kyej1023@gmail.com>
2026-01-19 16:09:14 +0000

    HTML Architecture
    - Added basic HTML file Architecture
    - Added HTML for buttons and display
    - Added header and footer
    - Added basic functions in .js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// Operation functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// Calculation Func
+function operate(a, b, operand) {
+    
+}
